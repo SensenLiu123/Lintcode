9083045b8192af8da6f6cdfb5f2582b05bcab88d
Sensen Liu <36674622+SensenLiu123@users.noreply.github.com>
2019-04-21 23:13:11 -0500

    Add files via upload

---
+++ 626.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: sensenliu
+"""
+
+"""
+Definition for a point.
+class Point:
+    def __init__(self, a=0, b=0):
+        self.x = a
+        self.y = b
+"""
+
+class Solution:
+    """
+    @param l1: top-left coordinate of first rectangle
+    @param r1: bottom-right coordinate of first rectangle
+    @param l2: top-left coordinate of second rectangle
+    @param r2: bottom-right coordinate of second rectangle
+    @return: true if they are overlap or false
+    """
+    def doOverlap(self, l1, r1, l2, r2):
+        # x-aixs no overlap
+        # two cases: rec1 rec 2 or rec 2 rec 1
+        if r1.x < l2.x or r2.x < l1.x:
+            return False 
+        # y-axis no overlap 
+        # also two cases 
+        if r1.y > l2.y or r2.y > l1.y:
+            return False 
+        
+        return True 
+        
