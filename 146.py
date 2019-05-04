314048ed1580765eed5178de265335c654408f74
Sensen Liu <36674622+SensenLiu123@users.noreply.github.com>
2019-05-04 18:56:20 -0500

    Add files via upload

---
+++ 146.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: sensenliu
+"""
+
+class Solution:
+    """
+    @param str: A string
+    @return: A string
+    """
+    def lowercaseToUppercase2(self, string):
+        # write your code here
+        return string.upper()
