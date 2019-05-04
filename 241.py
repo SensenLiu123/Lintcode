a8ebe2b08e7f5a474c55a921a490f934f8cf9df7
Sensen Liu <36674622+SensenLiu123@users.noreply.github.com>
2019-05-04 18:56:58 -0500

    Add files via upload

---
+++ 241.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: sensenliu
+"""
+
+class Solution:
+    """
+    @param str: A string
+    @return: An integer
+    """
+    def stringToInteger(self, string):
+        # write your code here
+        return int(string)
