13fd70c3981b683f5bcef76b1401e778663c6c7d
Sensen Liu <36674622+SensenLiu123@users.noreply.github.com>
2019-05-04 19:00:23 -0500

    Add files via upload

---
+++ 455.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: sensenliu
+"""
+class Student:
+    def __init__(self, id):
+        self.id = id;
+
+class Class:
+
+    '''
+     * Declare a constructor with a parameter n which is the total number of
+     * students in the *class*. The constructor should create n Student
+     * instances and initialized with student id from 0 ~ n-1
+    '''
+    def __init__(self, n):
+        self.students = [Student(id) for id in range(n)]
+
